e978b3699378c51156fe8a4878cf8f3db38d8bc8
Jack Gardner <jackmcggardner@outlook.com>
2023-10-16 20:48:17 +1100

    Adding furniture moving

--- App.jsx
+++ App.jsx
@@ -2,49 +2,49 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-// Check local storage for saved values. If not present, default to '100'.
-const initialWidth = localStorage.getItem('rectangleWidth') || '100';
-const initialLength = localStorage.getItem('rectangleLength') || '100';
-const initialZoom = localStorage.getItem('zoomLevel') ? parseFloat(localStorage.getItem('zoomLevel')) : 1;
+  // Check local storage for saved values. If not present, default values are set.
+  const initialWidth = localStorage.getItem('rectangleWidth') || '500';
+  const initialLength = localStorage.getItem('rectangleLength') || '500';
+  const initialZoom = localStorage.getItem('zoomLevel') ? parseFloat(localStorage.getItem('zoomLevel')) : 1;
+  const initialFurniture = JSON.parse(localStorage.getItem('furnitureData')) || [];
 
-// State for width and length, zoom and furniture.
-const [width, setWidth] = useState(initialWidth);
-const [length, setLength] = useState(initialLength);
-const [zoom, setZoom] = useState(initialZoom);
-// const [furniture, setFurniture] = useState([]);
+  // State for room dimensions, zoom, and furniture items.
+  const [width, setWidth] = useState(initialWidth);
+  const [length, setLength] = useState(initialLength);
+  const [zoom, setZoom] = useState(initialZoom);
+  const [furniture, setFurniture] = useState(initialFurniture);
 
-const initialFurniture = JSON.parse(localStorage.getItem('furnitureData')) || [];
-const [furniture, setFurniture] = useState(initialFurniture);
+  // Save room dimensions, zoom level, and furniture details to local storage when they change.
+  useEffect(() => {
+    localStorage.setItem('rectangleWidth', width);
+    localStorage.setItem('rectangleLength', length);
+    localStorage.setItem('zoomLevel', zoom.toString());
+    localStorage.setItem('furnitureData', JSON.stringify(furniture));
+  }, [width, length, zoom, furniture]);
 
-// Save width and length values to local storage whenever they change.
-useEffect(() => {
-  localStorage.setItem('rectangleWidth', width);
-  localStorage.setItem('rectangleLength', length);
-  localStorage.setItem('zoomLevel', zoom.toString());
-  localStorage.setItem('furnitureData', JSON.stringify(furniture));
-}, [width, length, zoom, furniture]);
+  const addFurniture = () => {
+    const newFurniture = { id: Date.now(), name: '', length: '', width: '', x: 0, y: 0 };  // Added x, y default positions
+    setFurniture([...furniture, newFurniture]);
+  };
 
-const addFurniture = () => {
-  const newFurniture = { id: Date.now(), name: '', length: '', width: '' };
-  setFurniture([...furniture, newFurniture]);
-};
+  const updateFurniture = (id, field, value) => {
+    setFurniture(furniture.map(item => item.id === id ? { ...item, [field]: value } : item));
+  };
 
-const updateFurniture = (id, field, value) => {
-  setFurniture(furniture.map(item => item.id === id ? { ...item, [field]: value } : item));
-};
-
-const removeFurniture = (id) => {
-  setFurniture(furniture.filter(item => item.id !== id));
-};
+  const removeFurniture = (id) => {
+    setFurniture(furniture.filter(item => item.id !== id));
+  };
 
   return (
     <div className="app">
 
       <div className="control-section">
+
         <h2>Measurements</h2>
-        {/* Zoom controls */}
+
         <button onClick={() => setZoom(prevZoom => Math.min(prevZoom + 0.1, 3))}>Zoom In</button>
         <button onClick={() => setZoom(prevZoom => Math.max(prevZoom - 0.1, 0.5))}>Zoom Out</button>
+
         <h3>Room:</h3>
         <div className="input-section">
           <div className="input-wrapper">
@@ -72,31 +72,24 @@ const removeFurniture = (id) => {
         </div>
 
         <h3>Furniture</h3>
+
           <button onClick={addFurniture}>+</button>
+          
           {furniture.map(item => (
             <div key={item.id} className="furniture-section">
-              <div className="name-input-group">
-                {/* <div className="input-wrapper">
-                  <label htmlFor={`name-${item.id}`}>Name:</label>
-                  <input 
-                    id={`name-${item.id}`}
-                    placeholder="Name"
-                    value={item.name}
-                    onChange={(e) => updateFurniture(item.id, 'name', e.target.value)}
-                  />
-                </div> */}
-                <div className="input-wrapper standard-input">
-                  <label htmlFor={`name-${item.id}`}>Name:</label>
-                  <input 
-                    id={`name-${item.id}`}
-                    placeholder="Name"
-                    value={item.name}
-                    onChange={(e) => updateFurniture(item.id, 'name', e.target.value)}
-                  />
+            <div className="name-input-group">
+              <div className="input-wrapper standard-input">
+                <label htmlFor={`name-${item.id}`}>Name:</label>
+                <input 
+                  id={`name-${item.id}`}
+                  placeholder="Name"
+                  value={item.name}
+                  onChange={(e) => updateFurniture(item.id, 'name', e.target.value)}
+                />
                 </div>
-
                 <button className="remove-button" onClick={() => removeFurniture(item.id)}>-</button>
               </div>
+              
               <div className="dimension-input-group">
                 <div className="input-wrapper">
                   <label htmlFor={`length-${item.id}`}>Length (cm):</label>
@@ -119,6 +112,31 @@ const removeFurniture = (id) => {
                   />
                 </div>
               </div>
+
+              {/* New position input fields */}
+              <div className="position-input-group">
+                <div className="input-wrapper">
+                  <label htmlFor={`x-${item.id}`}>X Position (px):</label>
+                  <input 
+                    id={`x-${item.id}`}
+                    type="number"
+                    placeholder="X"
+                    value={item.x}
+                    onChange={(e) => updateFurniture(item.id, 'x', e.target.value)}
+                  />
+                </div>
+                <div className="input-wrapper">
+                  <label htmlFor={`y-${item.id}`}>Y Position (px):</label>
+                  <input 
+                    id={`y-${item.id}`}
+                    type="number"
+                    placeholder="Y"
+                    value={item.y}
+                    onChange={(e) => updateFurniture(item.id, 'y', e.target.value)}
+                  />
+                </div>
+              </div>
+
             </div>
           ))}
 
@@ -138,13 +156,33 @@ const removeFurniture = (id) => {
               width: `${width}px`,
               height: `${length}px`,
               border: '1px solid white',
+              position: 'relative',
             }}
-          ></div>
+          >
+            {furniture.map(item => (
+              <div
+                key={item.id}
+                className="furniture-rectangle"
+                style={{
+                  width: `${item.length}px`,
+                  height: `${item.width}px`,
+                  border: '1px solid red',
+                  position: 'absolute',
+                  top: `${item.y}px`,
+                  left: `${item.x}px`,
+                }}
+              >
+                <div className="furniture-length-label">{item.name} - {item.length} cm</div> 
+                <div className="furniture-width-label">{item.name} - {item.width} cm</div> 
+              </div>
+            ))}
+          </div>
           <div className="width-label">Room - {length} cm</div>
         </div>
+
       </div>
     </div>
-  );
+);
 }
 
 export default App;
